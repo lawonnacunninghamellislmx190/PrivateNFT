34fb3a75657c4c863f52d0907d6d201e0b2dc07c
lawonnacunninghamellislmx190 <lawonnacunninghamellislmx190@hotmail.com>
2024-08-27 05:22:54 +0000

    Initialised

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("git commit -m "Initialised"","git push origin main")
+        ERC721("zz","zz")
         Ownable(initialOwner)
     {}
 
